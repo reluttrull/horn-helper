6d4275364ea4455062214acdfb6964e2b4ceb288
Ryan Luttrull <reluttrull@gmail.com>
2025-07-19 14:04:19 -0500

    fix img urls

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
