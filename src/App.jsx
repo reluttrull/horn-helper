b6165b58b0c09044cd4ac4a63975ecaaa68a9bc8
Ryan Luttrull <reluttrull@gmail.com>
2025-07-21 13:16:00 -0500

    added tab stubs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FaBookOpen, FaChartLine, FaGear } from 'react-icons/fa6';
 import './App.css'
 import { FingeringPractice } from './components/FingeringPractice.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const Tabs = {
+    FINGERINGPRACTICE: 'fingeringPractice',
+    SETTINGS: 'settings',
+    MYLEADERBOARD: 'myLeaderboard',
+    GLOBALLEADERBOARD: 'globalLeaderboard'
+  };
+  const [tab, setTab] = useState(Tabs.FINGERINGPRACTICE);
 
   return (
     <>
-      <FingeringPractice />
+      <button onClick={() => {setTab(Tabs.FINGERINGPRACTICE)}}><FaBookOpen /></button>
+      <button onClick={() => {setTab(Tabs.SETTINGS)}}><FaGear /></button>
+      <button onClick={() => {setTab(Tabs.MYLEADERBOARD)}}><FaChartLine /></button>
+      { tab == Tabs.FINGERINGPRACTICE ? <FingeringPractice /> : <div></div>}
+      { tab == Tabs.SETTINGS ? <h2>Settings</h2> : <div></div>}
+      { tab == Tabs.MYLEADERBOARD ? <h2>My Leaderboard</h2> : <div></div>}
     </>
   )
 }
