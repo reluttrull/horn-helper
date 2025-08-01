1ba36d76f937f9be48b1511466566f6de918d8e4
Ryan Luttrull <reluttrull@gmail.com>
2025-08-01 14:32:37 -0500

    other necessary pieces for vite deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/horn-helper/'
 })
